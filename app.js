6cea406bcebc35851422eed80c65a7b861019a0a
stharuma <stharuma@gmail.com>
2018-12-05 14:32:36 -0500

    first commit :- created dummy REST Api

---
+++ app.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const logger = require('morgan'); 
+
+
+const app =express();
+
+//Middlewares
+app.use(logger('dev'));
+//Routes
+app.get('/', (req, res, next) =>{
+  res.status(200).json({
+      message : 'You requested index page'
+  })
+});
+
+//Catch 404 errors and forward them to the error handeler
+app.use((req, res, next) => {
+ const err = new Error("Not Found");
+ err.status = 404;
+ next(err);
+});
+//Error Handeler function
+app.use((err, req, res, next) => {
+    const error = app.get('env')==='development' ? err : {};
+    const status = err.status || 500;
+    //Respond to client
+    res.status(status).json({
+        error:{
+            message:error.message
+        }
+    });
+    //Respond to ourselves
+    console.error(err);
+});
+//start the server
+const port =app.get('port') || 3000;
+app.listen(port, () => console.log(`Server is listening on ${port}`));
