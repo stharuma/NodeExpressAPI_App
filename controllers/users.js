58e79efb8034202bcd42cd2d97a5504d814b255a
stharuma <stharuma@gmail.com>
2018-12-18 12:44:12 -0500

    removed console log

--- controllers/users.js
+++ controllers/users.js
@@ -1,6 +1,7 @@
  const User = require('../models/user');
 
  module.exports = {
+     //Callback
      index: (req, res, next) => {
          User.find({}, (err, users) => {
              if (err) {
@@ -9,14 +10,9 @@
              res.status(200).json(users);
          })
      },
-
      newUser: (req, res, next) => {
-         console.log('request body content ', req.body);
          const newUser = new User(req.body);
-         console.log('new User ', newUser);
          newUser.save((err, user) => {
-             console.log('err', err);
-             console.log('user', user);
              res.status(201).json(user);
          });
      }
