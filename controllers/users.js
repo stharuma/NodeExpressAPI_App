09065f4edbd7570d29dbe98f30f981cef8b51e05
stharuma <stharuma@gmail.com>
2018-12-18 16:05:17 -0500

    added express-promise-router and removed all try catch block in async/await function

--- controllers/users.js
+++ controllers/users.js
@@ -2,22 +2,15 @@
 
  module.exports = {
      // Async/Await
-    index: async (req, res, next) => {
-         try {
-             const users = await User.find({});
-             res.status(200).json(users);
-         } catch (err) {
-             next(err);
-         }
+     index: async (req, res, next) => {
+         const users = await User.find({});
+         // throw new Error('dummy error'); 
+         res.status(200).json(users);
      },
      newUser: async (req, res, next) => {
-         try {
-             const newUser = new User(req.body);
-             const user =await newUser.save();
-             res.status(201).json(user);
-         } catch (error) {
-             next(err);
-         }
+         const newUser = new User(req.body);
+         const user = await newUser.save();
+         res.status(201).json(user);
      }
  };
 
