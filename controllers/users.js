6419232d108cb2bc8592d0f369cf6525a265a1b0
stharuma <stharuma@gmail.com>
2018-12-18 15:33:30 -0500

    added Async/await function

--- controllers/users.js
+++ controllers/users.js
@@ -1,26 +1,23 @@
  const User = require('../models/user');
 
  module.exports = {
-  // Promises
-     index: (req, res, next) => {
-        User.find({})
-        .then((users)=>{
-        res.status(200).json(users);
-        })
-        .catch(err=>{
-                next(err);
-            });
-     },
-     newUser: (req, res, next) => {
-         const newUser = new User(req.body);
-         newUser.save()
-         .then(user => {
-             res.status(201).json(user);
-         })
-         .catch(err =>{
+     // Async/Await
+    index: async (req, res, next) => {
+         try {
+             const users = await User.find({});
+             res.status(200).json(users);
+         } catch (err) {
              next(err);
-         });
-         
+         }
+     },
+     newUser: async (req, res, next) => {
+         try {
+             const newUser = new User(req.body);
+             const user =await newUser.save();
+             res.status(201).json(user);
+         } catch (error) {
+             next(err);
+         }
      }
  };
 
